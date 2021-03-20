3107444bd07728825c11efb4a07b48f72ce083ab
kovidd <49589399+kovidd@users.noreply.github.com>
2021-03-21 10:27:57 +1100

    Main code file

--- gameplay.py
+++ gameplay.py
@@ -13,9 +13,11 @@ def play():
     p1 = Player(validate_input('Enter the number of ships you want to place on board: '))  # Select number of ships to be placed on board
     p1.make_fleet()
 
-    for _ in range(validate_input('Enter the number of missiles you want to fire: ')):  # Input the number of missiles to be fired
-        while p1.fleet:  # Play till not all ships are destroyed
-            p1.attack()
+    missiles = (validate_input('Enter the number of missiles you want to fire: '))  # Input the number of missiles to be fired
+    counter = 1
+    while p1.fleet and counter <= missiles:  # Play till not all ships are destroyed
+        p1.attack()
+        counter += 1
 
     if not p1.fleet:
         print('\033[4m\x1b[31mPlayer has lost!\033[0m\x1b[0m')  # No ships left on board, player lost
